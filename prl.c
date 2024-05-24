7276c378d177424bab667246540b2557d6a94e6a
nayt2608 <nayt2608@gmail.com>
2024-05-24 10:28:46 +0700

    All files to new repo

---
+++ prl.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
+int main() {
+	uid_t userid;
+	gid_t groupid;
+	pid_t procid, parentid;
+	userid = getuid();
+	groupid = getgid();
+	procid = getpid();
+	parentid = getppid();
+	printf("Ид, пользователя: %d\n" userid);
+	printf("Ид,группы пользователя: %d\n" groupid);
+	printf("Ид, текущего процесса: %d\n" procid);
+	printf("Ид, родительского процесса: %d\n" parentid);
+	return 0;
+}
